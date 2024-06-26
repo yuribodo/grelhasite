3fc221629940c93423b682bbf44ffd13d60a4e0b
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-25 21:09:58 -0400

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className="bg-gray-800 text-white text-center p-4">
+        <p>&copy; 2024 Nossa Loja. Todos os direitos reservados.</p>
+      </div>
+    );
+  }
+  
+  export default Footer;

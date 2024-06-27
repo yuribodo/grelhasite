d61b60d49841549de4bb82c5542fe4f62bef730f
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-27 19:00:19 -0400

    a

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
